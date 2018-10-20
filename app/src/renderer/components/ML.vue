2f1e3a2f1d280d004a17ca2064e88f03548a05e6
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2018-10-20 16:55:43 +0900

    Modelからコマンド推薦を実装

--- app/src/renderer/components/ML.vue
+++ app/src/renderer/components/ML.vue
@@ -3,6 +3,57 @@
 </template>
 
 <script>
+import model from "../assets/model.json"
+
+const list = [
+    {
+        type: "command",
+        val: "git"
+    },
+    {
+        type: "option",
+        val: "-l"
+    },
+    {
+        type: "pipe"
+    },
+    {
+        type: "command",
+        val: "grep",
+        args: [
+            {
+                placeholder: "ワード"
+            }
+        ]
+    },
+    {
+        type: "command",
+        val: "cat",
+        args: [
+            {
+                placeholder: "ファイル名"
+            }
+        ]
+    },
+    {
+        type: "command",
+        val: "ping",
+        args: [
+            {
+                placeholder: "ホスト名"
+            }
+        ]
+    },
+    {
+        type: "option",
+        val: "-n",
+        args: [
+            {
+                placeholder: "回数"
+            }
+        ]
+    }
+]
 export default {
     props: ["input"],
     data() {
@@ -21,59 +72,63 @@ export default {
         this.update()
     },
     methods: {
+
         update() {
-            const list = [
-                {
-                    type: "command",
-                    val: "ls"
-                },
-                {
-                    type: "option",
-                    val: "-l"
-                },
-                {
-                    type: "pipe"
-                },
-                {
-                    type: "command",
-                    val: "grep",
-                    args: [
-                        {
-                            placeholder: "ワード",
-                            val: ".json"
-                        }
-                    ]
-                },
-                {
-                    type: "command",
-                    val: "cat",
-                    args: [
-                        {
-                            placeholder: "ファイル名",
-                            val: "README.txt"
-                        }
-                    ]
-                },
-                {
-                    type: "command",
-                    val: "ping",
-                    args: [
-                        {
-                            placeholder: "ホスト名"
-                        }
-                    ]
-                },
-                {
-                    type: "option",
-                    val: "-n",
-                    args: [
-                        {
-                            placeholder: "回数"
-                        }
-                    ]
+            var p = []
+
+            
+            if(this.second in model && this.last in model[this.second]["next"]){
+                for(let key of Object.keys(model[this.second]["next"][this.last]["next"])){
+                    let obj = {
+                        type:model[this.second]["next"][this.last]["next"][key]["type"],
+                        val:model[this.second]["next"][this.last]["next"][key]["cmd"]["val"],
+                        args:model[this.second]["next"][this.last]["next"][key]["cmd"]["args"]
+                    }
+                    p.push(obj)
                 }
-            ]
+            }
+
+            console.log(p)
+            if(p.length!=0){
+                this.$emit("update", JSON.parse(JSON.stringify(p)))
+                return
+            }
+
+
+            if(this.last in model){
+                for(let key of Object.keys(model[this.last]["next"])){
+                    let obj = {
+                        type:model[this.last]["next"][key]["type"],
+                        val:model[this.last]["next"][key]["cmd"]["val"],
+                        args:model[this.last]["next"][key]["cmd"]["args"]
+                    }
+
+                    p.push(obj)
+                }
+            }
+            console.log(p)
+
+            if(p.length!=0){
+                this.$emit("update", JSON.parse(JSON.stringify(p)))
+                return
+            }
             this.$emit("update", list)
+        }
+    },
+    computed: {
+        cmdSplit:function(){
+            return this.input.split(" ").filter(function(data){
+                return data.length > 0
+            })
+        },
+        last:function(){
+            return this.cmdSplit[this.cmdSplit.length-1]
+        },
+        second:function(){
+            if(this.cmdSplit.length > 1){
+                return this.cmdSplit[this.cmdSplit.length - 2]
+            }
+            return ""
         }
     }
 }
