3aeeb946e38f7c17244228e4e3cdc192fe0baeaa
nobm <tyayu@icloud.com>
2018-10-21 02:32:13 +0900

    new block color!

--- app/src/renderer/components/Formula/Phrase.vue
+++ app/src/renderer/components/Formula/Phrase.vue
@@ -55,11 +55,12 @@ export default {
     text-align: center;
     box-shadow: 0 1px 2px rgb(92, 92, 92);
     z-index: 101;
+    text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
     padding-left: 10px;
 }
 
 .Option {
-    background: rgb(82, 223, 0);
+    background: rgb(0, 224, 86);
 }
 
 .Redirect {
