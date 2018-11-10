1ed8621683b477b48f23fc8c49d597e0706b9891
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2018-11-10 15:59:29 +0900

    コマンドの推薦方法の変更

--- app/src/renderer/components/ML.vue
+++ app/src/renderer/components/ML.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-  </div>
+    <div>
+    </div>
 </template>
 
 <script>
@@ -9,81 +9,6 @@ import cmdDescription from "../assets/cmdDescription.json"
 
 const list = [
     {
-        type: "command",
-        val: "cd",
-        description:cmdDescription["cd"],
-        args: [
-        {
-          placeholder: "移動先ディレクトリ"
-        }
-        ]
-    },
-    {
-        type: "command",
-        val: "touch",
-        description:cmdDescription["touch"],
-        args:[
-            {
-                placeholder:"ファイル名"
-            }
-        ]
-    },
-    {
-        type: "command",
-        val: "mkdir",
-        description:cmdDescription["mkdir"],
-        args:[
-            {
-                placeholder:"ディレクトリ名"
-            }
-        ]
-    },
-    {
-        type: "command",
-        val: "cat",
-        description:cmdDescription["cat"],
-        args: [
-            {
-                placeholder: "ファイル名"
-            }
-        ]
-    },
-    {
-        type:"command",
-        description:cmdDescription["rm"],
-        val:"rm"
-    },
-    {
-        type:"command",
-        description:cmdDescription["pwd"],
-        val:"pwd"
-    },
-    {
-        type:"command",
-        description:cmdDescription["ls"],
-        val:"ls"
-    },
-    {
-        type: "command",
-        val:"find",
-        description:cmdDescription["find"],
-        args:[
-            {
-                placeholder:"検索箇所"
-            }
-        ]
-    },
-    {
-        type:"command",
-        description:cmdDescription["vim"],
-        val:"vim",
-        args: [
-            {
-                placeholder: "ファイル名"
-            }
-        ]
-    },
-    {
         type:"command",
         description:cmdDescription["git"],
         val:"git"
@@ -91,10 +16,6 @@ const list = [
 ]
 export default {
     props: ["input","searchWord"],
-    data() {
-        return {
-        }
-    },
     watch: {
         input: {
             deep: true,
@@ -114,47 +35,32 @@ export default {
     },
     methods: {
         update() {
-            var p = []
+            let rec = []
+            let path = model
+            
+            for(let target of this.input){
+                if(target["val"] in path){
+                    path = path[target["val"]]["next"]
+                }
+            }
 
+            // console.log(path)
+
+            for(let cmd in path){
+                rec.push({
+                    val:cmd,
+                    description:cmdDescription["git"],
+                    type:path[cmd]["type"]
+                })
+            }
+
+            console.log(rec)
+            
             if(this.cmdSplit.length == 0){
-                this.$emit("update", JSON.parse(JSON.stringify(list)))
-                return
+                this.$emit("update",JSON.parse(JSON.stringify(list)))
             }
-        
-            if(this.second in model && this.last in model[this.second]["next"]){
-                for(let key of Object.keys(model[this.second]["next"][this.last]["next"])){
-                    let obj = {
-                        type:model[this.second]["next"][this.last]["next"][key]["type"],
-                        val:model[this.second]["next"][this.last]["next"][key]["cmd"]["val"],
-                        args:model[this.second]["next"][this.last]["next"][key]["cmd"]["args"],
-                        description:cmdDescription[key],
-                    }
-                    p.push(obj)
-                }
-            }
-
-
-            if(p.length!=0){
-                this.$emit("update", JSON.parse(JSON.stringify(p)))
-                return
-            }
-
-            if(this.last in model){
-                for(let key of Object.keys(model[this.last]["next"])){
-                    let obj = {
-                        type:model[this.last]["next"][key]["type"],
-                        val:model[this.last]["next"][key]["cmd"]["val"],
-                        args:model[this.last]["next"][key]["cmd"]["args"],
-                        description:cmdDescription[key]
-                    }
-
-                    p.push(obj)
-                }
-            }
-
-            if(p.length!=0){
-                this.$emit("update", JSON.parse(JSON.stringify(p)))
-                return
+            else if(rec.length != 0){
+                this.$emit("update", JSON.parse(JSON.stringify(rec)))
             }
             else{
                 this.$emit("update", [])
@@ -163,21 +69,7 @@ export default {
         search(){
             let searchList = []
 
-            if(this.searchWord.length == 0 ){
-                this.update()
-            }
-
-            for(let key of Object.keys(cmdDescription)){
-                if(this.searchWord.includes(key) || cmdDescription[key].includes(this.searchWord)){
-                    let obj = {
-                        type:"command",
-                        val:key,
-                        args:[],
-                        description:cmdDescription[key]
-                    }
-                    searchList.push(obj)
-                }
-            }
+            return
             
             if(this.searchWord != ""){
                 this.$emit("update",searchList)
@@ -185,15 +77,6 @@ export default {
         }
     },
     computed: {
-        last:function(){
-            return this.cmdSplit[this.cmdSplit.length-1]
-        },
-        second:function(){
-            if(this.cmdSplit.length > 1){
-                return this.cmdSplit[this.cmdSplit.length - 2]
-            }
-            return ""
-        },
         cmdSplit:function(){
             let cmd = []
             for(let item of this.input){
