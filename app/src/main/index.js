6ee48256900f5e2d6762b5212a7e276603d4163d
nobm <tyayu@icloud.com>
2018-11-17 22:02:33 +0900

    ペースト対応

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron"
+import { app, BrowserWindow,Menu } from "electron"
 
 /**
  * Set `__static` path to static files in production
@@ -37,6 +37,28 @@ function createWindow() {
     mainWindow.on("closed", () => {
         mainWindow = null
     })
+
+    // Create the Application's main menu
+    var template = [{
+        label: "Eterm",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 let subWindow
@@ -136,3 +158,6 @@ ipcMain.on('onfocus', (event) => {
 ipcMain.on('onblur', (event) => {
     subWindow.hide();
 })
+
+
+
